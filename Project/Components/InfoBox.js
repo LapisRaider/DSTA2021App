2639194a2cce5024812439a79244019642ecca64
LapisRaider <angielimenqi7@gmail.com>
2021-06-14 18:49:22 +0800

    - infoBox component

---
+++ Project/Components/InfoBox.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import { Text, Title } from "react-native-paper";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+/*
+    Should show a title, and information that I can just insert in
+*/
+const InfoBox = ({ title, innerText }) => {
+  console.log(title);
+  return (
+    <View style={styles.mainContainer}>
+      <Title style={styles.titleStyle}>{title}</Title>
+      <Text style={styles.innerText}>{innerText}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    backgroundColor: "#FFC300",
+    width: "90%",
+    borderRadius: 15,
+    padding: "3%",
+    alignContent: "flex-start",
+  },
+  titleStyle: {
+    color: "black",
+    fontWeight: "bold",
+  },
+  innerText: {
+    color: "black",
+  },
+});
+
+export default InfoBox;
