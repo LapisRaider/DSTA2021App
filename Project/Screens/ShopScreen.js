44c937841ce5b4b169f85ef044a6d1871880ca4a
TypeDefinition <lnxterry@gmail.com>
2021-06-14 23:46:32 +0800

    Added ShopCard

--- Project/Screens/ShopScreen.js
+++ Project/Screens/ShopScreen.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import ShopCard from '../Components/ShopCard.js';
 
 const ShopScreen = () => {
   return (
     <SafeAreaView style={styles.mainContainer}>
       <View style={styles.contentContainer}>
-        <Text>Shop Screen</Text>
+        <ShopCard
+          title="Rabbit Meat"
+          collectionTime="2021-10-14, 1600hrs"
+          collectionPoint="Blk 851 Hougang Central, #13-24"
+          currentMoney={450.0}
+          goalMoney={500.0}
+        />
       </View>
     </SafeAreaView>
   );
