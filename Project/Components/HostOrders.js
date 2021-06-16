62b8df80cbf9b797ecf573128d3bf12c53d32644
JocelynOng <jocelyn.ong0212@gmail.com>
2021-06-16 17:52:45 +0800

    profile

---
+++ Project/Components/HostOrders.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Text, View, StyleSheet, Image } from 'react-native';
+
+export default function HostOrders({imageUri, name, closingDate}) {
+  return (
+    <View style={{ height: 150, width: 200, marginLeft: 20, borderWidth: 1, borderColor: '#FF9D2C' }}>
+        <View style={{ flex: 2 }}>
+            <Image source={imageUri} style={{ flex: 1, width: null, height: null, resizeMode: 'cover' }}/>
+        </View>
+        <View style={{ flex: 1, paddingLeft: 10, paddingTop: 10 }}>
+            <Text>{name}</Text>
+            <Text>Jio-end: {closingDate}</Text>
+        </View>
+    </View>
+        
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
--- Project/Screens/ProfileScreen.js
+++ Project/Screens/ProfileScreen.js
@@ -1,27 +1,61 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import { Text } from 'react-native-paper';
+import { StyleSheet, View, ScrollView, Text, Image } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import Header from '../Components/Header';
+import HostOrders from '../Components/HostOrders';
+import BuyerOrders from '../Components/HostOrders';
+import PastOrders from '../Components/HostOrders';
 
 const ProfileScreen = () => {
   return (
+    
     <SafeAreaView style={styles.mainContainer}>
-      <View style={styles.contentContainer}>
-        <Text>Profile Screen</Text>
-      </View>
+      <Header title='MY JIOs' />
+      <ScrollView scrollEventThrottle={10}>
+        <View style={{flex: 1, backgroundColor: 'white', paddingTop: 20}}> 
+          <View style={{flex:1, marginTop:20}}>
+            <Text style={styles.jioHeader}> JIO-ing</Text>
+            <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+              <HostOrders imageUri={require('../Images/rabbit-meat.jpeg')} name='Rabbit Meat' closingDate='20 June 2021' />
+              <HostOrders imageUri={require('../Images/burnt-ends.jpeg')} name='Burnt Ends Bakery' closingDate='21 June 2021'/>
+              <HostOrders imageUri={require('../Images/whisking-bakes.jpg')} name='Whisking Bakes' closingDate='22 June 2021'/>
+            </ScrollView>
+          </View>
+          <View style={{marginTop:40}}>
+            <Text style={styles.jioHeader}> JIO-coming </Text>
+            <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+              <BuyerOrders imageUri={require('../Images/rabbit-meat.jpeg')} name='Rabbit Meat2' />
+              <BuyerOrders imageUri={require('../Images/burnt-ends.jpeg')} name='Burnt Ends Bakery2' />
+              <BuyerOrders imageUri={require('../Images/whisking-bakes.jpg')} name='Whisking Bakes2' />
+            </ScrollView>
+          </View>
+          <View style={{marginTop:40}}>
+            <Text style={styles.jioHeader}> JIO-ed </Text>
+            <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+              <PastOrders imageUri={require('../Images/rabbit-meat.jpeg')} name='Rabbit Meat3' />
+              <PastOrders imageUri={require('../Images/burnt-ends.jpeg')} name='Burnt Ends Bakery3' />
+              <PastOrders imageUri={require('../Images/whisking-bakes.jpg')} name='Whisking Bakes3' />
+            </ScrollView>
+          </View>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
-};
+}
+
 
 const styles = StyleSheet.create({
   mainContainer: {
-    flex: 1
-  },
-  contentContainer: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
+    backgroundColor:'white',
+  },
+  jioHeader: {
+    fontSize: 18, 
+    fontWeight: 'bold',
+    paddingHorizontal: 20,
+    paddingBottom: 5,
+  },
+
 });
 
 export default ProfileScreen;
